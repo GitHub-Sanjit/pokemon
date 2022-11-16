d4c61eb277abbd22814cafd7e225d5a0e601b68f
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-16 17:50:17 +0600

    4. Styling PokeCard and Pokedex

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import "./Pokecard.css";
 const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
 
- class Pokecard extends Component {
+class Pokecard extends Component {
   render() {
     let imgSrc = `${POKE_API}${this.props.id}.png`;
 
     return (
       <div className="Pokecard">
-        <h1>{this.props.name}</h1>
+        <h1 className="Pokecard-title">{this.props.name}</h1>
         <img src={imgSrc} alt={this.props.name} />
-        <div>Type: {this.props.type}</div>
-        <div>Exp: {this.props.exp}</div>
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">Exp: {this.props.exp}</div>
       </div>
     );
   }
